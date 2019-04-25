66bce8c85a4e8e833773987522bcc4f86acb7492
Patrick Willner <patrick.willner@tum.de>
2019-04-25 19:58:19 +0200

    add input parameters

--- cuda/sim_cuda.cpp
+++ cuda/sim_cuda.cpp
@@ -4,40 +4,107 @@
 using namespace ldpc;
 
 // /usr/local/cuda-9.2/bin/nvcc -x cu -std=c++11 sim_cuda.cpp ldpcsim.cpp ldpc/ldpc.cpp ldpc/decoder.cpp device/cudamgd.cpp device/kernel.cpp device/ldpcsimdevice.cpp  -o sim_cuda -arch sm_35 -rdc=true -O3 -w
-int main()
+int main(int argc, char *argv[])
 {
-/*
-    //set up code class on unified memory
-    ldpc_code* code = new ldpc_code("../src/code/test_code/code_rand_proto_3x6_400_4.txt", "../src/code/test_code/layer_rand_proto_3x6_400_4.txt", true);
+	bool abort = false;
 
-	cudamgd_ptr<ldpc_code> code(code_dev);
+	std::string codeFile;
+	std::string simFile;
+	std::string mapFile;
+	std::string layerFile;
+	uint16_t numThreads;
 
-    //set up simulation
-    ldpc_sim sim(code_dev, "../src/sim.txt", "../src/code/test_code/mapping_rand_proto_3x6_400_4.txt");
-    sim.print();
-    sim.start();
-*/
+	if (argc == 7)
+	{
+		for (int i = 0; i < (argc - 1) / 2; ++i)
+		{
+			if (strcmp(argv[2 * i + 1], "-code") == 0)
+			{
+				codeFile.assign(argv[2 * i + 2]);
+			}
+			else if (strcmp(argv[2 * i + 1], "-sim") == 0)
+			{
+				simFile.assign(argv[2 * i + 2]);
+			}
+			else if (strcmp(argv[2 * i + 1], "-map") == 0)
+			{
+				mapFile.assign(argv[2 * i + 2]);
+			}
+			else
+			{
+				abort = true;
+			}
+		}
+	}
+	else if (argc == 9 || argc == 11)
+	{
+		for (int i = 0; i < (argc - 1) / 2; ++i)
+		{
+			if (strcmp(argv[2 * i + 1], "-code") == 0)
+			{
+				codeFile.assign(argv[2 * i + 2]);
+			}
+			else if (strcmp(argv[2 * i + 1], "-sim") == 0)
+			{
+				simFile.assign(argv[2 * i + 2]);
+			}
+			else if (strcmp(argv[2 * i + 1], "-map") == 0)
+			{
+				mapFile.assign(argv[2 * i + 2]);
+			}
+			else if (strcmp(argv[2 * i + 1], "-layer") == 0)
+			{
+				layerFile.assign(argv[2 * i + 2]);
+			}
+			else if (strcmp(argv[2 * i + 1], "-threads") == 0)
+			{
+				numThreads = static_cast<unsigned short>(atoi(argv[2 * i + 1]));
+			}
+			else
+			{
+				abort = true;
+			}
+		}
+	}
+	else
+	{
+		abort = true;
+	}
+
+	if (abort)
+	{
+		std::cout << "======================== LDPC Simulation ========================\n";
+		std::cout << "                        (Layered Decoding)                       \n";
+		std::cout << "                         Usage Reminder:                         \n";
+		std::cout << "         Main -code CodeFile -sim SimFile -map MappingFile       \n";
+		std::cout << "               optional: -layer LayerFile                        \n";
+		std::cout << "                         -threads NumThreads                     \n";
+		std::cout << "                                                                 \n";
+		std::cout << "                                                                 \n";
+		std::cout << "                CodeFile: Name of the code file                  \n";
+		std::cout << "               CodeMapFile: Name of mapping file                 \n";
+		std::cout << "               SimFile: Name of simulation file                  \n";
+		std::cout << "              LayerFile: Name of code layer file                 \n";
+		std::cout << "                         for layered decoding                    \n";
+		std::cout << "              NumThreads: Number of threads for parallel         \n";
+		std::cout << "                          processing of frames (default: 1)      \n";
+		std::cout << "=================================================================" << std::endl;
+		exit(EXIT_FAILURE);
+	}
 
 	cudamgd_ptr<ldpc_code_device> code_dev(
 		ldpc_code_device(
-			"../src/code/test_code/code_rand_proto_3x6_400_4.txt"
-			, "../src/code/test_code/layer_rand_proto_3x6_400_4.txt"
-			, true
-		)
-	);
+			codeFile.c_str(), layerFile.c_str()));
 
 	cudamgd_ptr<ldpc_sim_device> sim_dev(
 		ldpc_sim_device(
-			code_dev
-			, "../src/sim.txt"
-			, "../src/code/test_code/mapping_rand_proto_3x6_400_4.txt"
-		)
-	);
-	//sim_dev->print();
+			code_dev, simFile.c_str(), mapFile.c_str()));
+
+	sim_dev->print();
 	//sim_dev->start();
 	sim_dev->start_device();
-	
-/*
+
+	/*
     //set up decoder on unified memory
 	try
 	{
@@ -59,5 +126,5 @@ int main()
 		std::cout << "Error ldpc_decoder_device" << '\n';
 	}
 */
-    return 0;
+	return 0;
 }
